34f5d7f7223332bf06b065fe0559008adbdb5e39
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2020-08-26 13:45:47 -0700

    Added google map

--- react-frontend-01-starting-setup/src/places/pages/UserPlaces.js
+++ react-frontend-01-starting-setup/src/places/pages/UserPlaces.js
@@ -12,7 +12,7 @@ const DUMMY_PLACES = [
         address: '20 W 34th St, New York, NY 10001',
         location: {
             lat: 40.7484,
-            long: -73.9857
+            lng: -73.9857
         },
         creator: 'u1'
     },
@@ -24,7 +24,7 @@ const DUMMY_PLACES = [
         address: '20 W 34th St, New York, NY 10001',
         location: {
             lat: 40.7484,
-            long: -73.9857
+            lng: -73.9857
         },
         creator: 'u2'
     }
