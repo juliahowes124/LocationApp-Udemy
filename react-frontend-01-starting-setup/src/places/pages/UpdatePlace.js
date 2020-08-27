b82e6644cbebf29955856c6bbae87e3e2cbee5dc
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2020-08-27 11:59:15 -0700

    Made custom form hook for update and new

--- react-frontend-01-starting-setup/src/places/pages/UpdatePlace.js
+++ react-frontend-01-starting-setup/src/places/pages/UpdatePlace.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../../shared/util/validators';
+import { useForm } from '../../shared/hooks/form-hook';
 import './PlaceForm.css';
 
 const DUMMY_PLACES = [
@@ -34,9 +35,41 @@ const DUMMY_PLACES = [
 ]
 
 const UpdatePlace = () => {
+    const [isLoading, setIsLoading] = useState(true);
     const placeId = useParams().placeId;
 
+    const [formState, inputHandler, setFormData] = useForm({
+        title: {
+            value: '',
+            isValid: false
+        },
+        description: {
+            value: '',
+            isValid: false
+        }
+    }, false);
+
+    const placeUpdateSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs);
+    } 
+
     const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId);
+
+    useEffect(() => {
+        setFormData({
+            title: {
+                value: identifiedPlace.title,
+                isValid: true
+            },
+            description: {
+                value: identifiedPlace.description,
+                isValid: true
+            }
+        }, true);
+        setIsLoading(false);
+    }, [setFormData, identifiedPlace]);
+    
 
     if (!identifiedPlace) {
         return (
@@ -46,30 +79,41 @@ const UpdatePlace = () => {
         );
     }
 
-    return <form className="place-form">
-        <Input
-            id="title"
-            element="input"
-            type="text"
-            label="Title"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText="A title is required"
-            onInput={() => {}}
-            value={identifiedPlace.title}
-            valid={true}
-        />
-        <Input
-            id="description"
-            element="textarea"
-            label="Description"
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText="Please enter a valid description of at least 5 characters."
-            onInput={() => {}}
-            value={identifiedPlace.description}
-            valid={true}
-        />
-        <Button type="submit" disabled={true}>UPDATE PLACE</Button>
-    </form>
+    if (isLoading) {
+        return (
+            <div className="center">
+                <h2>Loading...</h2>
+            </div>
+        )
+    }
+    
+
+    return (
+            <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
+            <Input
+                id="title"
+                element="input"
+                type="text"
+                label="Title"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="A title is required"
+                onInput={inputHandler}
+                initialValue={formState.inputs.title.value}
+                initialValid={formState.inputs.title.isValid}
+            />
+            <Input
+                id="description"
+                element="textarea"
+                label="Description"
+                validators={[VALIDATOR_MINLENGTH(5)]}
+                errorText="Please enter a valid description of at least 5 characters."
+                onInput={inputHandler}
+                initialValue={formState.inputs.description.value}
+                initialValid={formState.inputs.description.isValid}
+            />
+            <Button type="submit" disabled={!formState.isValid}>UPDATE PLACE</Button>
+        </form>
+    )
 };
 
 export default UpdatePlace;
