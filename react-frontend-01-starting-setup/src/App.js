aef5fe7430b46bd1730b867cc52500a47be38a82
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2020-08-27 14:27:26 -0700

    Added global auth context

--- react-frontend-01-starting-setup/src/App.js
+++ react-frontend-01-starting-setup/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useCallback} from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
 import Users from './user/pages/Users';
@@ -7,33 +7,66 @@ import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
 import UpdatePlace from './places/pages/UpdatePlace';
+import { AuthContext } from './shared/context/auth-context';
 
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
+  let routes;
+  if (isLoggedIn) {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:uid/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/places/new" exact>
+          <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+      
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+        <Route path="/:uid/places" exact>
+          <UserPlaces />
+        </Route>
+        <Route path="/auth">
+          <Auth />
+        </Route>
+        <Redirect to="/auth" />
+      </Switch>
+      
+    );
+  }
+
+
   return (
-  <Router>
+  <AuthContext.Provider value={{isLoggedIn: isLoggedIn, login: login, logout: logout}}>
+    <Router>
     <MainNavigation/>
     <main>
-    <Switch>
-      <Route path="/" exact>
-        <Users />
-      </Route>
-      <Route path="/:uid/places" exact>
-        <UserPlaces />
-      </Route>
-      <Route path="/places/new" exact>
-        <NewPlace />
-      </Route>
-      <Route path="/places/:placeId">
-        <UpdatePlace />
-      </Route>
-      <Route path="/auth">
-        <Auth />
-      </Route>
-      <Redirect to="/"/>
-    </Switch>
+      {routes}
     </main>
   </Router>
+  </AuthContext.Provider>
   )
 }
 
--- react-frontend-01-starting-setup/src/user/pages/Auth.js
+++ react-frontend-01-starting-setup/src/user/pages/Auth.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
 import Card from '../../shared/components/UIElements/Card';
 import {VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE} from '../../shared/util/validators';
 import { useForm } from '../../shared/hooks/form-hook';
+import { AuthContext } from '../../shared/context/auth-context';
 import './Auth.css';
 
 const Auth = () => {
+    const auth = useContext(AuthContext);
     const [isLogin, setIsLogin] = useState(true);
     const [formState, inputHandler, setFormData] = useForm({
         email: {
@@ -41,6 +43,7 @@ const Auth = () => {
     const authSubmitHandler = event => {
         event.preventDefault();
         console.log(formState.inputs);
+        auth.login();
     } 
 
     return (
