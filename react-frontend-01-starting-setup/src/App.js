a4b1a32b0d656be159b22e3f0d2d77d7a5adeb6d
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2020-08-27 13:24:04 -0700

    Added login form

--- react-frontend-01-starting-setup/src/App.js
+++ react-frontend-01-starting-setup/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
 import Users from './user/pages/Users';
+import Auth from './user/pages/Auth';
 import NewPlace from './places/pages/NewPlace';
 import UserPlaces from './places/pages/UserPlaces';
 import MainNavigation from './shared/components/Navigation/MainNavigation';
@@ -25,6 +26,9 @@ function App() {
       </Route>
       <Route path="/places/:placeId">
         <UpdatePlace />
+      </Route>
+      <Route path="/auth">
+        <Auth />
       </Route>
       <Redirect to="/"/>
     </Switch>
