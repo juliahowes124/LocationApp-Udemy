5adc83f3e00a5189858fa6e8f56947b036a2b5bb
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2020-09-08 17:41:07 -0700

    Added error handling and two get routes for places

--- node-backend-01-basic-prj-setup/routes/places-routes.js
+++ node-backend-01-basic-prj-setup/routes/places-routes.js
@@ -1,10 +1,67 @@
 const express = require('express');
+const HttpError = require('../models/http-error');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    console.log('GET Request in Places');
-    res.json({message: 'It works!'});
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'famous building',
+        location: {
+            lat: 40.7,
+            lng: -73
+        },
+        address: '123 poopoo st. New York, NY 10001',
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building',
+        description: 'famous building',
+        location: {
+            lat: 40.7,
+            lng: -73
+        },
+        address: '123 poopoo st. New York, NY 10001',
+        creator: 'u1'
+    }
+];
+
+router.get('/:pid', (req, res, next) => {
+    const placeId = req.params.pid;
+    const place = DUMMY_PLACES.find(p => {
+        return p.id == placeId;
+    });
+
+    if (!place) {
+        return next(new HttpError('Could not find a place for the provided place id.', 404));
+    }
+    res.json({place});
 });
+
+router.get('/user/:uid', (req, res, next) => {
+    const userId = req.params.uid;
+    const userPlaces = [];
+
+
+    userCheck = DUMMY_PLACES.find(p => {
+        return p.creator == userId;
+    });
+    if (!userCheck) {
+        return next(new Error('Could not find a place for the provided user id.', 404));
+    }
+
+
+    for (i=0; i<DUMMY_PLACES.length; i++) {
+        const placeToAppend = DUMMY_PLACES.find(p => {
+            return p.creator == userId;
+        });
+        userPlaces.push(placeToAppend);
+    }
+   
+    
+    res.json({userPlaces});
+})
 
 module.exports = router;
