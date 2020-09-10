45456c139b3a920277e5c9b5259a336be8aadd1b
juliahowes124 <65035431+juliahowes124@users.noreply.github.com>
2020-09-10 14:44:11 -0700

    Input validation in API and use Google API to convert address to coordinates

--- node-backend-01-basic-prj-setup/controllers/places-controllers.js
+++ node-backend-01-basic-prj-setup/controllers/places-controllers.js
@@ -1,5 +1,7 @@
 const HttpError = require('../models/http-error');
 const { v4: uuid } = require('uuid');
+const { validationResult } = require('express-validator');
+const getCoordsForAddress = require('../util/location');
 
 let DUMMY_PLACES = [
     {
@@ -52,8 +54,20 @@ const getPlacesByUserId = (req, res, next) => {
     res.json({userPlaces});
 };
 
-const createPlace = (req, res, next) => {
-    const {title, description, coordinates, address, creator} = req.body;
+const createPlace = async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return next(new HttpError('Invalid inputs passed, please check your data', 422));
+    }
+    const {title, description, address, creator} = req.body;
+
+    let coordinates;
+    try {
+        coordinates = await getCoordsForAddress(address);
+    } catch (error) {
+        return next(error);
+    }
+    
     const createdPlace = {
         id: uuid(),
         title,
@@ -70,6 +84,9 @@ const createPlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
     const placeId = req.params.pid;
+    if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+        throw new HttpError('Could not find a place for that id', 404);
+    }
     DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
 
     res.status(200).json({message: 'Deleted place.'});
@@ -77,6 +94,12 @@ const deletePlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log(errors);
+        throw new HttpError('Invalid inputs passed, please check your data', 422);
+    }
+    
     const placeId = req.params.pid;
     const placeForUpdate = { ...DUMMY_PLACES.find(p => p.id === placeId) };
     const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
--- node-backend-01-basic-prj-setup/routes/users-routes.js
+++ node-backend-01-basic-prj-setup/routes/users-routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const { check } = require('express-validator');
 const usersControllers = require('../controllers/users-controllers');
 const router = express.Router();
 
 router.get('/', usersControllers.getUsers);
-router.post('/signup', usersControllers.signUp);
+router.post('/signup',
+    [
+        check('name').not().isEmpty(),
+        check('email').normalizeEmail().isEmail(), //normalizeEmail makes Test@test.com turn to test@test.com
+        check('password').isLength({ min:6 })
+
+    ],
+    usersControllers.signUp);
 router.post('/login', usersControllers.login);
 
 module.exports = router;
